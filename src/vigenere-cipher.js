46a88bb6ba00ae7543c92aaca686c0bfd975b59f
Pr1nceska <lubitel.priklu4eniy1103@gmail.com>
2025-01-07 02:42:54 +0300

    feat: complete vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,35 +1,64 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  processMessage(msg, key, isEncrypt) {
+    if (!msg || !key) throw new Error("Incorrect arguments!");
+
+    const res = [];
+    const message = msg.toUpperCase();
+    const k = key.toUpperCase();
+    let keyIndex = 0;
+
+    [...message].forEach((char) => {
+      if (char.match(/[A-Z]/)) {
+        const mi = char.charCodeAt(0) - 65;
+        const ki = k[keyIndex % k.length].charCodeAt(0) - 65;
+        const ci = isEncrypt
+          ? ((mi + ki) % 26) + 65
+          : ((mi - ki + 26) % 26) + 65;
+
+        res.push(String.fromCharCode(ci));
+        keyIndex++;
+      } else {
+        res.push(char);
+      }
+    });
+
+    return this.direct ? res.join("") : res.reverse().join("");
+  }
+
+  encrypt(msg, key) {
+    return this.processMessage(msg, key, true);
+  }
+
+  decrypt(msg, key) {
+    return this.processMessage(msg, key, false);
   }
 }
 
 module.exports = {
-  VigenereCipheringMachine
+  VigenereCipheringMachine,
 };
